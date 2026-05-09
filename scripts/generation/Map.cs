5a545a436d53ab8a8af1a0b753cb3b644362edf2
Kürşat "SessizLeylek" Kuyumcu <kursatkymcu@gmail.com>
2026-05-09 03:10:55 +0300

    Generate safe lines based on randomly selected points

--- scripts/generation/Map.cs
+++ scripts/generation/Map.cs
@@ -14,25 +14,66 @@ public class Map
         SingleObjects.Add(obj);
     }*/
 
-    public static void CreateMap(int seed)
+    public static void CreateMap(int seed, float newMapSize = 100f)
     {
+        float MapSize = newMapSize; // TODO: REMOVE THIS WHEN DECIDED
+
         var random = new Random(seed);
 
         var map = new Map();
 
-        const int safeLineCount = 50;
-        List<SafeLine> safeLines = new(safeLineCount);
+        const int safePivotCount = 50;
+        List<Vector2> safePivots = new(safePivotCount);
 
-        // Generate the safe lines
-        for (var i = 0; i < safeLineCount; i++)
+        // Generate safe pivots
+        for (var i = 0; i < safePivotCount; i++)
         {
-            var x0 = random.NextSingle() * 200 - 100;
-            var y0 = random.NextSingle() * 200 - 100;
-            var x1 = random.NextSingle() * 200 - 100;
-            var y1 = random.NextSingle() * 200 - 100;
-            var line = new SafeLine(new Vector2(x0, y0), new Vector2(x1, y1));
+            var x = (random.NextSingle() * 2 - 1) * MapSize;
+            var y = (random.NextSingle() * 2 - 1) * MapSize;
+            safePivots[i] = new Vector2(x, y);
+        }
 
-            safeLines.Add(line);
+        List<SafeLine> safeLines = new();
+        int[] safePivotConnectionCounts = new int[safePivotCount];
+
+        // Generate the safe lines between safe pivots
+        for (var i = 0; i < safePivotCount; i++)
+        {
+            const int maxPivotConnections = 2;
+            List<int> candidatePivotIndices = new();
+            for (var pivotIndex = 0; pivotIndex < safePivotCount; pivotIndex++)
+            {
+                if (safePivotConnectionCounts[pivotIndex] < maxPivotConnections)
+                {
+                    candidatePivotIndices.Add(pivotIndex);
+                }
+            }
+
+            for (var j = safePivotConnectionCounts[i]; j < maxPivotConnections; j++)
+            {
+                if (candidatePivotIndices.Count > 0)
+                {
+                    var selectedPivotIndex = candidatePivotIndices[0];
+                    var bestDistanceSqr = float.MaxValue;
+                    foreach (var candidatePivotIndex in candidatePivotIndices)
+                    {
+                        var newDistanceSqr = safePivots[candidatePivotIndex].DistanceSquaredTo(safePivots[i]);
+                        if (newDistanceSqr < bestDistanceSqr)
+                        {
+                            bestDistanceSqr = newDistanceSqr;
+                            selectedPivotIndex = candidatePivotIndex;
+                        }
+                    }
+                    candidatePivotIndices.Remove(selectedPivotIndex);
+                    safePivotConnectionCounts[selectedPivotIndex]++;
+
+                    var pivot0 = safePivots[i];
+                    var pivot1 = safePivots[selectedPivotIndex];
+                    var line = new SafeLine(pivot0, pivot1);
+                    safeLines.Add(line);
+                }
+            }
+
         }
 
         map.SafeLines = safeLines;
